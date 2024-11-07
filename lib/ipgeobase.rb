925cf9e067c5d1d2b02d9e605ec867487212aa20
Aleksey Elyseev <aelyseev@gmail.com>
2024-11-07 12:46:56 +0300

    Fix lint issues

--- lib/ipgeobase.rb
+++ lib/ipgeobase.rb
@@ -1,14 +1,29 @@
 # frozen_string_literal: true
+
+# rubocop:disable Naming/MethodParameterName
+# rubocop:disable Naming/VariableName
+# rubocop:disable Naming/MethodName
+
 require "net/http"
 require "uri"
 require "addressable/uri"
-require 'happymapper'
+require "happymapper"
 
 require_relative "ipgeobase/version"
 
+# The Ipgeobase module provides methods to work with IP geolocation.
+# It includes functionality to retrieve geolocation
+# information based on a given IP address.
 module Ipgeobase
   API_BASE = "http://ip-api.com/"
 
+  # The LookupResult class represents the result of an IP address geolocation lookup.
+  # It includes attributes such as:
+  # - city: The name of the city.
+  # - country: The name of the country.
+  # - countryCode: The code of the country (e.g., "IT" for Italy).
+  # - lat: The geographical latitude.
+  # - lon: The geographical longitude.
   class LookupResult
     attr_reader :city, :country, :countryCode, :lat, :lon
 
@@ -25,31 +40,40 @@ module Ipgeobase
 
   def self.build_url(address)
     uri = Addressable::URI.parse(Ipgeobase::API_BASE)
-    if uri.respond_to?(:path)
-      uri.path = "xml/#{address}"
-    else
-      raise Error, "Can't build URL"
-    end
+    raise Error, "Can't build URL" unless uri.respond_to?(:path)
+
+    uri.path = "xml/#{address}"
+
     URI.parse(uri.normalize)
   end
 
-  def self.lookup(address)
-    response = Net::HTTP.get_response(build_url(address))
+  def self.make_request(uri)
+    response = Net::HTTP.get_response(uri)
 
-    xml_body = if response.is_a?(Net::HTTPSuccess)
-                 response.body
-               else
-                 raise Error, "Error: #{response.code} - #{response.message}"
-               end
+    raise Error, "Error: #{response.code} - #{response.message}" unless response.is_a?(Net::HTTPSuccess)
+
+    response.body
+  end
+
+  def self.parse_xml(xml)
     begin
-      ip_meta = HappyMapper::parse(xml_body)
+      meta = HappyMapper.parse(xml)
     rescue StandardError => e
       raise Error, "Error while parsing xml response: #{e.class} - #{e.message}"
     end
 
-    if ip_meta.status == "fail"
-      raise Error, ["Status: #{ip_meta.status}", "message: #{ip_meta.message}", "address: #{ip_meta.query}"].join(" ")
+    if meta.status == "fail"
+      raise Error, ["Status: #{meta.status}", "message: #{meta.message}", "address: #{meta.query}"].join(" ")
     end
+
+    meta
+  end
+
+  private_class_method :make_request, :parse_xml
+
+  def self.lookup(address)
+    xml_body = make_request(build_url(address))
+    ip_meta = parse_xml(xml_body)
 
     LookupResult.new(
       city: ip_meta.city,
@@ -60,3 +84,7 @@ module Ipgeobase
     )
   end
 end
+
+# rubocop:enable Naming/MethodParameterName
+# rubocop:enable Naming/VariableName
+# rubocop:enable Naming/MethodName
--- test/test_ipgeobase.rb
+++ test/test_ipgeobase.rb
@@ -5,10 +5,10 @@ require "webmock"
 
 class TestIpgeobase < Minitest::Test
   def setup
-    stub_request(:get, Ipgeobase::build_url("0.0.0.0").to_s).
-      to_return(status: 200, body: RESERVED_RANGE_RESPONSE, headers: {})
-    stub_request(:get, Ipgeobase::build_url("62.149.128.40").to_s).
-      to_return(status: 200, body: PUBLIC_IP_RESPONSE, headers: {})
+    stub_request(:get, Ipgeobase.build_url("0.0.0.0").to_s)
+      .to_return(status: 200, body: RESERVED_RANGE_RESPONSE, headers: {})
+    stub_request(:get, Ipgeobase.build_url("62.149.128.40").to_s)
+      .to_return(status: 200, body: PUBLIC_IP_RESPONSE, headers: {})
   end
 
   def test_that_it_has_a_version_number
@@ -17,12 +17,12 @@ class TestIpgeobase < Minitest::Test
 
   def test_return_error_for_reserved_addresses
     assert_raises(Ipgeobase::Error) do
-      Ipgeobase::lookup "0.0.0.0"
+      Ipgeobase.lookup "0.0.0.0"
     end
   end
 
   def test_return_proper_resut_valid_ip
-    meta = Ipgeobase::lookup "62.149.128.40"
+    meta = Ipgeobase.lookup "62.149.128.40"
     assert_kind_of String, meta.city
     assert_kind_of String, meta.country
     assert_kind_of String, meta.countryCode
